26ef55229c86a212e2d19cbae4b96f20bc644ff5
Photon <86115068+AnandY4d4v@users.noreply.github.com>
2026-01-21 16:32:12 +0530

    Update app.js

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080/merchants";
+const BASE_URL = "https://merchants-ggd5.onrender.com/merchants";
 let selectedMerchantId = null;
 
 document.addEventListener("DOMContentLoaded", loadMerchants);
